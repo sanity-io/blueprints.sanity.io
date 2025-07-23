bf456ca22f58d78238643963e0d930c13e257fb6
Brian LeRoux <brian.leroux@sanity.io>
2025-07-23 15:40:50 -0700

    fix: lang on jsonschema element

--- elements/bp-jsonschema.mjs
+++ elements/bp-jsonschema.mjs
@@ -3,5 +3,5 @@ import {join} from 'node:path'
 export default function jsonschema ({ html, state }) {
   let pathToPages = join(process.cwd(), 'pages', state.attrs.src)
   let json = fs.readFileSync(pathToPages).toString()
-  return html`<a href=${state.attrs.src}>raw</a><pre class=langauge-typescript>${ json }</pre>`
+  return html`<a href=${state.attrs.src}>raw</a><pre class=language-typescript>${ json }</pre>`
 }
