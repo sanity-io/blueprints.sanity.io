b52b4606bba9d67f309607034884f7b264cfebb7
Cole Peters <cole@colepeters.com>
2025-08-05 16:01:32 -0500

    Fix bp-code tab layout issue

--- ui/elements/bp-code.mjs
+++ ui/elements/bp-code.mjs
@@ -1,11 +1,18 @@
-export default function code ({ html, state }) {
+export default function code({ html, state }) {
   return html`
-    <input type=radio id="ts-${state.instanceID}" name=tab value=ts checked>
+    <input
+      type="radio"
+      id="ts-${state.instanceID}"
+      name="tab"
+      value="ts"
+      checked
+    />
     <label for="ts-${state.instanceID}">TS</label>
-    <slot name=ts></slot>
 
-    <input type=radio id="json-${state.instanceID}" name=tab value=json>
+    <input type="radio" id="json-${state.instanceID}" name="tab" value="json" />
     <label for="json-${state.instanceID}">JSON</label>
-    <slot name=json></slot>
+
+    <slot name="ts"></slot>
+    <slot name="json"></slot>
   `
 }
